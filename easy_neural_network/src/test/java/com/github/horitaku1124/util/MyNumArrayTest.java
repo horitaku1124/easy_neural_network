d4422f43d459a51baf44586523d3f19cd6d2333f
Horimatsu Takuya <new_woory@hotmail.com>
2018-09-20 18:51:26 +0900

    test: modify closeTo to harmcrest

--- easy_neural_network/src/test/java/com/github/horitaku1124/util/MyNumArrayTest.java
+++ easy_neural_network/src/test/java/com/github/horitaku1124/util/MyNumArrayTest.java
@@ -2,19 +2,11 @@ package com.github.horitaku1124.util;
 
 import org.junit.Test;
 
-import static java.lang.Math.abs;
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.closeTo;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class MyNumArrayTest {
-
-    private boolean isCloseTo(float actual, float expected) {
-        final float error = 0.001f;
-        return abs(actual - expected) < error;
-    }
-
     @Test
     public void test1() {
         MyNumArray num = new MyNumArray(2, 4);
@@ -27,10 +19,10 @@ public class MyNumArrayTest {
         num.set(7.0f, 1, 2);
         num.set(8.0f, 1, 3);
 
-        assertTrue(isCloseTo(num.get(0, 0), 1f));
-        assertTrue(isCloseTo(num.get(0, 3), 4f));
-        assertTrue(isCloseTo(num.get(1, 2), 7f));
-        assertTrue(isCloseTo(num.get(1, 3), 8f));
+        assertThat((double)num.get(0, 0), is(closeTo(1.0f, 0.001f)));
+        assertThat((double)num.get(0, 3), is(closeTo(4.0f, 0.001f)));
+        assertThat((double)num.get(1, 2), is(closeTo(7.0f, 0.001f)));
+        assertThat((double)num.get(1, 3), is(closeTo(8.0f, 0.001f)));
     }
     @Test
     public void test2() {
@@ -47,9 +39,9 @@ public class MyNumArrayTest {
         num.set(18.0f, 1, 1, 2);
         num.set(23.0f, 1, 2, 3);
 
-        assertTrue(isCloseTo(num.get(0, 0, 0), 1f));
-        assertTrue(isCloseTo(num.get(0, 2, 0), 9f));
-        assertTrue(isCloseTo(num.get(1, 2, 3), 23f));
+        assertThat((double)num.get(0, 0, 0), is(closeTo(1.0f, 0.001f)));
+        assertThat((double)num.get(0, 2, 0), is(closeTo(9.0f, 0.001f)));
+        assertThat((double)num.get(1, 2, 3), is(closeTo(23.0f, 0.001f)));
     }
     @Test
     public void test3() {
@@ -59,7 +51,7 @@ public class MyNumArrayTest {
         num.set(3.0f, 0, 1, 0, 0);
         num.set(4.0f, 1, 0, 0, 0);
         num.set(120.0f, 1, 2, 3, 4);
-        assertTrue(isCloseTo(num.get(0, 0, 0, 0), 1f));
-        assertTrue(isCloseTo(num.get(1, 2, 3, 4), 120f));
+        assertThat((double)num.get(0, 0, 0, 0), is(closeTo(1.0f, 0.001f)));
+        assertThat((double)num.get(1, 2, 3, 4), is(closeTo(120.0f, 0.001f)));
     }
 }
